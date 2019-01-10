d20b6f0f7dae49666bf989cd3563ba11dc21627b
a.lam.tuan <lamtuananh278@gmail.com>
2019-01-10 16:14:12 +0100

    waiter, order activity

--- app/src/main/java/lam/fooapp/Utils/Constant.java
+++ app/src/main/java/lam/fooapp/Utils/Constant.java
@@ -3,8 +3,8 @@ package lam.fooapp.Utils;
 public class Constant {
  //   public static final String SERVER_URL = "http://192.168.0.94:13001/";
  //public static final String SERVER_URL = "http://172.22.86.177:13001/";
-    public static final String SERVER_URL = "http://10.0.10.70:13001/";
-    public static final String MAIN_SERVER_URL = "http://10.0.10.70:12001/";
+    public static final String SERVER_URL = "http://10.0.10.71:13001/";
+    public static final String MAIN_SERVER_URL = "http://10.0.10.71:12001/";
     public static final Integer MAX_FOOD_COUNT_ONPAGE=5;
 //    public static final String SERVER_URL = "https://socket-io-chat.now.sh/";
 
